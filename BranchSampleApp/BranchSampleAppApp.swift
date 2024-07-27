b3b3e4854261eacbd55214a19ba885b4d65b5e39
0102JohnnyTd <rt2824.recruit.1022@gmail.com>
2024-07-27 10:45:06 +0900

    Initial Commit

---
+++ BranchSampleApp/BranchSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BranchSampleAppApp.swift
+//  BranchSampleApp
+//
+//  Created by Johnny Toda on 2024/07/27.
+//
+
+import SwiftUI
+
+@main
+struct BranchSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
